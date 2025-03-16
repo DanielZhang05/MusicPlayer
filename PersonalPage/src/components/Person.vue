0375e218a8529262c7602b19131727ea8719c352
DanielZhang05 <ig23234@bristol.ac.uk>
2025-03-16 15:31:35 +0000

    Learn How to use vue

---
+++ PersonalPage/src/components/Person.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="person">
+        <h2>Name: {{ name }} </h2>
+        <h2>Age: {{ age }}</h2>
+        <button @click="showTel">Check telephone</button>
+    </div>
+</template>
+
+
+<script lang="ts">
+export default {
+    name: "Person",
+    data(): { name: string; age: number; tel: string } {
+        return {
+            name: "Zhang",
+            age: 20,
+            tel: "3133333"
+        };
+    },
+    methods: {
+        showTel() {
+            alert(this.tel);
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
